b1adddc93b9fe58478ba8b0307bc72a19a76ab63
Digant <digantvyahalkar@gmail.com>
2025-08-10 08:50:49 +0530

    feat: Implement authentication module
    * Added User entity, repository, and login service
    * Created LoginController with /api/auth/login endpoint
    * Developed JWT utility class for token creation and validation

---
+++ src/main/java/com/redpluto/auth/LoginController.java
@@ -0,0 +1,20 @@
+package com.redpluto.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @PostMapping(path = "/api/auth/login", consumes = { "application/json", "application/xml" }, produces = {
+	    "application/json", "application/xml" })
+    public LoginResponse validate(@RequestBody LoginRequest loginRequest) {
+
+	return loginService.validateUser(loginRequest);
+    }
+}
