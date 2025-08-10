4817243c441b8620bbbbb4ad2c170e113d0e37a4
Digant <digantvyahalkar@gmail.com>
2025-08-10 08:45:46 +0530

    feat: Develop DTO classes for Request & Response
    * Added validation annotations to StudentRequest
    * Created builder pattern in StudentRequest for immutability
    * Added StudentResponse DTO to handle API responses

---
+++ src/main/java/com/redpluto/dto/StudentResponse.java
@@ -0,0 +1,52 @@
+package com.redpluto.dto;
+
+public class StudentResponse {
+
+    private String message;
+    private Long studentId;
+    private StudentRequest studentDetails;
+    private int statusCode;
+
+    public StudentResponse() {
+    }
+
+    public static StudentResponse getInstance() {
+	return new StudentResponse();
+    }
+
+    public String getMessage() {
+	return message;
+    }
+
+    public StudentResponse setMessage(String message) {
+	this.message = message;
+	return this;
+    }
+
+    public Long getStudentId() {
+	return studentId;
+    }
+
+    public StudentResponse setStudentId(Long studentId) {
+	this.studentId = studentId;
+	return this;
+    }
+
+    public StudentRequest getStudentDetails() {
+	return studentDetails;
+    }
+
+    public StudentResponse setStudentDetails(StudentRequest studentDetails) {
+	this.studentDetails = studentDetails;
+	return this;
+    }
+
+    public int getStatusCode() {
+	return statusCode;
+    }
+
+    public StudentResponse setStatusCode(int statusCode) {
+	this.statusCode = statusCode;
+	return this;
+    }
+}
