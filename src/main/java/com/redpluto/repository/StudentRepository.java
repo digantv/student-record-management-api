5a04f59bdf489040b7ddf83f062ace72da6843d7
Digant <digantvyahalkar@gmail.com>
2025-08-29 02:40:21 +0530

    fix: Change in package name
    * Changed package name to student

--- src/main/java/com/redpluto/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.redpluto.repository;
-
-import com.redpluto.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {}
